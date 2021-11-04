7aa9eebe7ff318f13cbab4a27273d7baa33b5212
LeandroFChaves <leandro.fchaves@gmail.com>
2021-11-04 18:43:33 -0300

    Criado a estrutura inicial do micro serviço de cursos

---
+++ ead-curso/src/main/java/br/com/ead/curso/EadCursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ead.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadCursoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EadCursoApplication.class, args);
+    }
+
+}
