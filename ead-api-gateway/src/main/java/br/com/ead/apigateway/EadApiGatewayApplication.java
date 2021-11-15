26a961611029e678607ad780fb542e9a677e46dd
LeandroFChaves <leandro.fchaves@gmail.com>
2021-11-15 20:58:13 -0300

    Criado o projeto ead-api-gateway

---
+++ ead-api-gateway/src/main/java/br/com/ead/apigateway/EadApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.ead.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EadApiGatewayApplication.class, args);
+    }
+
+}
