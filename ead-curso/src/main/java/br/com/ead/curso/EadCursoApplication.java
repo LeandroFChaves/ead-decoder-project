0b01a7e8213e31b3bddafeb373bf216a17d24d79
LeandroFChaves <leandro.fchaves@gmail.com>
2021-11-15 18:41:57 -0300

    Configurado os microservices clients para se registrar no service registry

--- ead-curso/src/main/java/br/com/ead/curso/EadCursoApplication.java
+++ ead-curso/src/main/java/br/com/ead/curso/EadCursoApplication.java
@@ -2,8 +2,10 @@ package br.com.ead.curso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EadCursoApplication {
 
     public static void main(String[] args) {
