aef85b85ef4b7cf58e4dc8a945eece3406315cf0
LeandroFChaves <leandro.fchaves@gmail.com>
2021-12-08 18:50:04 -0300

    Criado o projeto para centralizar as config dos ms

---
+++ ead-config-server/src/test/java/br/com/ead/configserver/EadConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ead.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EadConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
